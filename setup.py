bd50bb2727c29fd4dbbba5ca6a4a8d2d47df6274
John Giannelos <jgiannelos@mozilla.com>
2019-08-08 19:20:10 +0200

    Create setup.py definition to distribute package.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="webcompat_ml",
+    version="0.0.1",
+    url="https://github.com/mozilla/webcompat-ml",
+    author="John Giannelos",
+    author_email="jgiannelos@mozilla.com",
+    description="WebCompat machine learning pipeline",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ],
+    license="MPL 2.0",
+)
